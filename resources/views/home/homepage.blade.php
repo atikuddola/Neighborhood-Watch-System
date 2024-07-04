1af9866d728de6e6e641d7ead65426ce3c158246
MdNafizurRahmanBhuiya <nafinbhuiya2021@gmail.com>
2024-07-05 01:33:21 +0600

    updated homepage

--- resources/views/home/homepage.blade.php
+++ resources/views/home/homepage.blade.php
@@ -166,10 +166,10 @@
     
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-63bf"><div class="u-clearfix u-sheet u-sheet-1">
-        <p class="u-small-text u-text u-text-variant u-text-1">Sample text. Click to select the Text Element.</p>
+        <p class="u-small-text u-text u-text-variant u-text-1">Macbook dorkar</p>
       </div></footer>
     <section class="u-backlink u-clearfix u-grey-80">
-    <p class="u-small-text u-text u-text-variant u-text-1">Macbook dorkar</p>
+    <p class="u-small-text u-text u-text-variant u-text-1">Sample text. Click to select the Text Element.</p>
     </section>
   
 </body></html>
